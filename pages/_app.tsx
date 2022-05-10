b25805e57795dccd59c89074d20b0299325e0bf2
avivashishta29 <avivashishta@gmail.com>
2022-05-10 19:43:47 +0530

    cursor fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import "../styles/globals.css";
 import { useEffect } from "react";
 
-function MyApp({ Component, pageProps }) {
-  var cursor;
-  var cursor2;
+function MyApp({ Component, pageProps }: any) {
+  var cursor: any;
+  var cursor2: any;
 
   useEffect(() => {
     cursor = document.getElementById("cursor");
@@ -18,9 +18,22 @@ function MyApp({ Component, pageProps }) {
       (cursor2.style.left = e.clientX + "px"),
         (cursor2.style.top = e.clientY + "px");
     });
+
+    document.addEventListener("mousedown", function (e) {
+      (cursor.style.height = "50px"), (cursor.style.width = "50px");
+    });
+    document.addEventListener("mouseup", function (e) {
+      (cursor.style.height = "35px"), (cursor.style.width = "35px");
+    });
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <div className="cursor2" id="cursor2"></div>
+      <div className="cursor" id="cursor"></div>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
