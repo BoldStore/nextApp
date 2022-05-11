187489911b2bd2529201c764f36f46536e9818a6
avivashishta29 <avivashishta@gmail.com>
2022-05-11 21:56:12 +0530

    carousel changes

--- components/HomePageComponents/AppWalkthrough/Phone2/index.js
+++ components/HomePageComponents/AppWalkthrough/Phone2/index.js
@@ -4,12 +4,7 @@ import { motion } from "framer-motion";
 
 function Phone2({ imgSrc, num, text, left }) {
   return (
-    <motion.div
-      className={styles.divFlex}
-      initial={{ scale: 0 }}
-      animate={{ scale: 1 }}
-      transition={{ delay: 0, duration: 0.5, type: "spring" }}
-    >
+    <motion.div className={styles.divFlex}>
       <motion.div className={styles.imgFlex}>
         <motion.div className={styles.circle} initial={{ scale: 1 }} />
         <motion.img src={imgSrc} initial={{ scale: 0.75, rotate: 0 }} />
