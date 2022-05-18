c2fddfc9d9be51d51e832b6bfb3a2bd8e72f5f88
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-18 22:56:19 +0530

    Test

--- pages/test.tsx
+++ pages/test.tsx
@@ -4,10 +4,29 @@
 import { NextPage } from "next";
 import { useSelector, useDispatch } from "react-redux";
 import { checkLogin, pingServer } from "../store/actions/test";
+import {
+  useAuthState,
+  useCreateUserWithEmailAndPassword,
+  useSignInWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+import { auth } from "../firebaseConfig";
+import { useState } from "react";
 
 const Test: NextPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const dispatch = useDispatch();
   const test = useSelector((state: any) => state.test);
+
+  const [userInSession] = useAuthState(auth);
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const [signInWithEmailAndPassword, loginUser, loginLoading, loginError] =
+    useSignInWithEmailAndPassword(auth);
+
   const pingServerTest = () => {
     dispatch(pingServer());
   };
@@ -16,9 +35,21 @@ const Test: NextPage = () => {
     dispatch(checkLogin());
   };
 
+  const signup = () => {
+    createUserWithEmailAndPassword(email, password);
+  };
+
+  const login = () => {
+    signInWithEmailAndPassword(email, password);
+  };
+
+  const logout = () => {
+    auth.signOut();
+  };
+
   return (
     <div>
-      <h1>This is a test to ping server</h1>
+      <h1>{`This is a test to ping server - Hello, ${userInSession}`}</h1>
       <button onClick={pingServerTest}>
         {test.isLoading ? "Loading..." : "Ping Server"}
       </button>
@@ -28,10 +59,49 @@ const Test: NextPage = () => {
       <h3>Message: {test.message}</h3>
 
       <br />
+      <h1>
+        Signup and Login -{" "}
+        {loading ? "Signup Loading" : loginLoading ? "Login Loading" : ""}
+      </h1>
+      <h3 style={{ color: "red" }}>
+        {error
+          ? "Signup error - " + error
+          : loginError
+          ? "Login Error - " + loginError
+          : ""}
+      </h3>
+      <h2>{`User - ${user?.user.uid || loginUser?.user.uid}`}</h2>
+      <form>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" onClick={login}>
+          Login
+        </button>
+        <button type="button" onClick={signup}>
+          Signup
+        </button>
+      </form>
+
+      <br />
       <h1>Check Login</h1>
       <button onClick={loginCheck}>Check</button>
       <h2>{test.success ? "Success" : !test.id ? "Failed" : "Not sent"}</h2>
       <h3>Message: {test.message}</h3>
+
+      <br />
+      <button onClick={logout}>Logout</button>
     </div>
   );
 };
