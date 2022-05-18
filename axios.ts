5ab2d09fe150952aaf366e8672fb88c8d76e01ff
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-05-18 21:09:35 +0530

    Auth header for every req

--- axios.ts
+++ axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { API_URL } from "./constants";
+import { auth } from "./firebaseConfig";
 
 const instance = axios.create({
   timeout: 15000,
@@ -8,6 +9,21 @@ const instance = axios.create({
     "Content-Type": "application/json",
     accept: "application/json",
   },
+});
+
+instance.interceptors.request.use(async (request) => {
+  const user = auth.currentUser;
+
+  if (user) {
+    try {
+      const token = await user.getIdToken();
+      request.headers!["Authorization"] = "Bearer " + token;
+    } catch (e) {
+      console.log("There was an error", e);
+    }
+  }
+
+  return request;
 });
 
 export default instance;
