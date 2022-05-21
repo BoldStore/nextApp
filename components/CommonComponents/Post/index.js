e62358d9696a037e0fdbf38bb7e6eb72353cf26c
aryanteng <aryanteng892@gmail.com>
2022-05-21 23:03:32 +0530

    post component anf grid component done

--- components/CommonComponents/Post/index.js
+++ components/CommonComponents/Post/index.js
@@ -2,18 +2,48 @@ import React from "react";
 import styles from "./styles.module.css";
 import Avatar from "@mui/material/Avatar";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import Image from "next/image";
+import { Bookmark } from "react-feather";
 
-function Post() {
+function Post({ storeName, storeLocation, postUrl, price, caption }) {
   return (
     <div className={styles.postContainer}>
       <div className={styles.postHeader}>
-        <Avatar
-          alt="Avatar"
-          src={"https://i.ibb.co/Bswp8RS/avi.jpg"}
-          sx={{ width: 50, height: 50, cursor: "pointer" }}
-        />
-        <MoreHorizIcon />
+        <div className={styles.userInfo}>
+          <Avatar
+            alt="Avatar"
+            src={"https://i.ibb.co/Bswp8RS/avi.jpg"}
+            sx={{
+              width: 50,
+              height: 50,
+              cursor: "pointer",
+              border: "1px solid var(--darkGrey)",
+            }}
+          />
+          <div className={styles.nameLocation}>
+            <p>Store_Username</p>
+            <p style={{ opacity: 0.5 }}>New Delhi</p>
+          </div>
+        </div>
+        <MoreHorizIcon className={styles.moreIcon} />
       </div>
+      <Image
+        src="/assets/shoe2.jpg"
+        alt="item"
+        width="450"
+        height="450"
+        className={styles.productImg}
+      />
+      <div className={styles.priceContainer}>
+        <p>$200</p>
+        <Bookmark className={styles.bookmarkIcon} />
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
     </div>
   );
 }
--- pages/trial.js
+++ pages/trial.js
@@ -1,11 +1,13 @@
 import React from "react";
 import BoldButton from "../components/CommonComponents/BoldButton";
 import Post from "../components/CommonComponents/Post";
+import Grid from "../components/Grid";
 
 function trial() {
   return (
     <div style={{ width: "80%", marginLeft: "auto", marginRight: "auto" }}>
-      <Post />
+      {/* <Grid /> */}
+      {/* <Post /> */}
     </div>
   );
 }
