b748c1f9cc256a5163b31f0a4c5a7d3508320b96
avivashishta29 <avivashishta@gmail.com>
2022-05-24 13:08:35 -0400

    store profile

---
+++ pages/store/profile/index.js
@@ -0,0 +1,39 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import Grid1 from "../../../components/CommonComponents/Grids/grid1";
+import Grid2 from "../../../components/CommonComponents/Grids/grid2";
+import Grid3 from "../../../components/CommonComponents/Grids/grid3";
+import Grid4 from "../../../components/CommonComponents/Grids/grid4";
+import CustomerHeader from "../../../components/CustomerComponents/Header";
+import styles from "./styles.module.css";
+
+function StoreProfile() {
+  return (
+    <>
+      <CustomerHeader />
+      <div className={styles.container}>
+        <div className={styles.storeDetails}>
+          <Avatar
+            alt="Avatar"
+            src={"https://i.ibb.co/myvq6GR/aryan.jpg"}
+            sx={{
+              width: 100,
+              height: 100,
+              cursor: "pointer",
+              border: "2px solid var(--darkGrey)",
+            }}
+          />
+          <h1>@store_profile</h1>
+        </div>
+        <div className={styles.productsGrid}>
+          <Grid1 />
+          <Grid2 />
+          <Grid3 />
+          <Grid4 />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default StoreProfile;
