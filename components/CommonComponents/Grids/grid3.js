88f00c2116b8d41170175a8a9af9a4e892dcb412
avivashishta29 <avivashishta@gmail.com>
2022-06-03 13:40:36 -0400

    opacity

--- components/CommonComponents/Grids/grid3.js
+++ components/CommonComponents/Grids/grid3.js
@@ -1,38 +1,154 @@
+import { motion } from "framer-motion";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles3.module.css";
 function Grid3() {
+  const [flag1, setFlag1] = useState(false);
+  const [flag2, setFlag2] = useState(false);
+  const [flag3, setFlag3] = useState(false);
+  const [flag4, setFlag4] = useState(false);
+  const [flag5, setFlag5] = useState(false);
+  const [flag6, setFlag6] = useState(false);
+
   return (
     <div className={styles.gridWrapper}>
       <div className={styles.parent}>
         <Link href="/store/product/1" passHref={true}>
-          <div className={styles.div1}>
+          <div
+            className={styles.div1}
+            onMouseEnter={() => setFlag1(true)}
+            onMouseLeave={() => setFlag1(false)}
+          >
             <img className={styles.bigImg} src={"/assets/shoe1.jpg"} />
+            {flag1 && (
+              <motion.div
+                className={styles.hoverDiv}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1, rotate: 0 }}
+                transition={{
+                  duration: 0.5,
+                  type: "spring",
+                  bounce: 0.4,
+                }}
+              >
+                <p>$100</p> <p>M</p>
+              </motion.div>
+            )}
           </div>
         </Link>
         <Link href="/store/product/1" passHref={true}>
-          <div className={styles.div2}>
+          <div
+            className={styles.div2}
+            onMouseEnter={() => setFlag2(true)}
+            onMouseLeave={() => setFlag2(false)}
+          >
             <img className={styles.img} src={"/assets/shoe1.jpg"} />
+            {flag2 && (
+              <motion.div
+                className={styles.hoverDiv}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1, rotate: 0 }}
+                transition={{
+                  duration: 0.5,
+                  type: "spring",
+                  bounce: 0.4,
+                }}
+              >
+                <p>$100</p> <p>M</p>
+              </motion.div>
+            )}
           </div>
         </Link>
         <Link href="/store/product/1" passHref={true}>
-          <div className={styles.div3}>
+          <div
+            className={styles.div3}
+            onMouseEnter={() => setFlag3(true)}
+            onMouseLeave={() => setFlag3(false)}
+          >
             <img className={styles.img} src={"/assets/shoe1.jpg"} />
+            {flag3 && (
+              <motion.div
+                className={styles.hoverDiv}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1, rotate: 0 }}
+                transition={{
+                  duration: 0.5,
+                  type: "spring",
+                  bounce: 0.4,
+                }}
+              >
+                <p>$100</p> <p>M</p>
+              </motion.div>
+            )}
           </div>
         </Link>
         <Link href="/store/product/1" passHref={true}>
-          <div className={styles.div4}>
+          <div
+            className={styles.div4}
+            onMouseEnter={() => setFlag4(true)}
+            onMouseLeave={() => setFlag4(false)}
+          >
             <img className={styles.img} src={"/assets/shoe1.jpg"} />
+            {flag4 && (
+              <motion.div
+                className={styles.hoverDiv}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1, rotate: 0 }}
+                transition={{
+                  duration: 0.5,
+                  type: "spring",
+                  bounce: 0.4,
+                }}
+              >
+                <p>$100</p> <p>M</p>
+              </motion.div>
+            )}
           </div>
         </Link>
         <Link href="/store/product/1" passHref={true}>
-          <div className={styles.div5}>
+          <div
+            className={styles.div5}
+            onMouseEnter={() => setFlag5(true)}
+            onMouseLeave={() => setFlag5(false)}
+          >
             <img className={styles.img} src={"/assets/shoe1.jpg"} />
+            {flag5 && (
+              <motion.div
+                className={styles.hoverDiv}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1, rotate: 0 }}
+                transition={{
+                  duration: 0.5,
+                  type: "spring",
+                  bounce: 0.4,
+                }}
+              >
+                <p>$100</p> <p>M</p>
+              </motion.div>
+            )}
           </div>
         </Link>
         <Link href="/store/product/1" passHref={true}>
-          <div className={styles.div6}>
+          <div
+            className={styles.div6}
+            onMouseEnter={() => setFlag6(true)}
+            onMouseLeave={() => setFlag6(false)}
+          >
             <img className={styles.bigImg} src={"/assets/shoe1.jpg"} />
+            {flag6 && (
+              <motion.div
+                className={styles.hoverDiv}
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1, rotate: 0 }}
+                transition={{
+                  duration: 0.5,
+                  type: "spring",
+                  bounce: 0.4,
+                }}
+              >
+                <p>$100</p> <p>M</p>
+              </motion.div>
+            )}
           </div>
         </Link>
       </div>
