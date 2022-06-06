89a22256302a9636af2a669088f841cf6a309a47
avivashishta29 <avivashishta@gmail.com>
2022-06-06 11:54:18 -0400

    update details

---
+++ components/StoreComponents/SignupComplete/AddressCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./styles.module.css";
+import { Truck } from "react-feather";
+import BoldButton from "../../CommonComponents/BoldButton";
+function AddressCard() {
+  return (
+    <div className={styles.wrapper}>
+      <Truck />
+      <h2 style={{ marginTop: "1rem" }}>Pick Up Details</h2>
+      <p style={{ color: "var(--lightGrey)" }}>
+        Fill in the address rom where you'll be shipping your orders
+      </p>
+      <BoldButton text={"Update"} href="store/profile/address" />
+    </div>
+  );
+}
+
+export default AddressCard;
--- components/StoreComponents/SignupComplete/index.js
+++ components/StoreComponents/SignupComplete/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./styles.module.css";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import UpiCard from "./UpiCard";
+import AddressCard from "./AddressCard";
 function SignUpComplete() {
   return (
     <div className={styles.container}>
@@ -19,6 +21,23 @@ function SignUpComplete() {
             trailColor: "#d6d6d6",
           })}
         />
+        <h2 style={{ textAlign: "center", marginTop: "1rem" }}>
+          Complete Your Profile
+        </h2>
+        <p style={{ textAlign: "center" }}>
+          <span style={{ color: "#1DA1F2" }}>2 out of 4</span> completed
+        </p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        <UpiCard />
+        <AddressCard />
       </div>
     </div>
   );
