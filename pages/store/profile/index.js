1e30db0e9a21dd28ced1d89d1083ebfd9f5a9297
avivashishta29 <avivashishta@gmail.com>
2022-06-06 11:55:15 -0400

    store profile done

--- pages/store/profile/index.js
+++ pages/store/profile/index.js
@@ -81,29 +81,31 @@ function StoreProfile({ user }) {
         <div className={styles.tabs}>
           <TabsStoreProfile />
         </div>
-        {value == 0 ? (
-          // <div className={styles.products}>
-          //   <div className={styles.productsGrid}>
-          //     <Grid1 />
-          //     <Grid2 />
-          //     <Grid3 />
-          //     <Grid4 />
-          //   </div>
-          // </div>
-          <SignUpComplete />
-        ) : value == 1 ? (
-          // <div className={styles.postContainer}>
-          //   <Post />
-          //   <Post />
-          //   <Post />
-          //   <Post />
-          //   <Post />
-          //   <Post />
-          // </div>
-          <SignUpComplete />
-        ) : (
-          <></>
-        )}
+        <div className={styles.desktopTabs}>
+          {value == 0 ? (
+            // <div className={styles.products}>
+            //   <div className={styles.productsGrid}>
+            //     <Grid1 />
+            //     <Grid2 />
+            //     <Grid3 />
+            //     <Grid4 />
+            //   </div>
+            // </div>
+            <SignUpComplete />
+          ) : value == 1 ? (
+            // <div className={styles.postContainer}>
+            //   <Post />
+            //   <Post />
+            //   <Post />
+            //   <Post />
+            //   <Post />
+            //   <Post />
+            // </div>
+            <SignUpComplete />
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </>
   );
