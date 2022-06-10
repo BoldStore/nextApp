33b2754e586cf6e01ae8adea0d400546266727b7
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-06-10 12:18:43 +0530

    Store profile

--- pages/store/[username].js
+++ pages/store/[username].js
@@ -1,11 +1,10 @@
 import { User } from "react-feather";
 import VerticalHeader from "../../components/StoreComponents/VerticalHeader";
 import styles from "./profile/styles.module.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TabsStoreProfile from "./profile/tabs";
 import VerifiedIcon from "@mui/icons-material/Verified";
-import SignUpComplete from "../../components/StoreComponents/SignupComplete";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Grid1 from "../../components/CommonComponents/Grids/grid1";
 import Grid2 from "../../components/CommonComponents/Grids/grid2";
 import Grid3 from "../../components/CommonComponents/Grids/grid3";
@@ -13,13 +12,35 @@ import Grid4 from "../../components/CommonComponents/Grids/grid4";
 import Post from "../../components/CommonComponents/Post";
 import StoreHeader from "../../components/StoreComponents/Header";
 import CustomerHeader from "../../components/CustomerComponents/Header";
+import { useRouter } from "next/router";
+import { storePage } from "../../store/actions/pages";
+import Loading from "../../components/Loading";
+import { Avatar } from "@mui/material";
 
 function StorePage() {
+  const { query } = useRouter();
+  const dispatch = useDispatch();
+  const store = useSelector((state) => state.pages);
   const profile = useSelector((state) => state.profile);
   const [value, setValue] = useState(0);
   const handleChange = (i) => {
     setValue(i);
   };
+
+  useEffect(() => {
+    if (query?.username) {
+      dispatch(storePage(query.username));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  if (store.store_loading) {
+    return <Loading />;
+  }
+
+  if (store.store_errmess) {
+    return <div>{store.store_errmess}</div>;
+  }
 
   return (
     <>
@@ -32,10 +53,10 @@ function StorePage() {
       />
       <div className={styles.container}>
         <div className={styles.storeDetails}>
-          {profile.profile_pic ? (
+          {store?.store?.store?.profile_pic ? (
             <Avatar
-              alt="Avatar"
-              src={profile.profile_pic}
+              alt="Store Profile Picture"
+              src={store.store.store.profile_pic}
               sx={{
                 width: 100,
                 height: 100,
@@ -62,7 +83,7 @@ function StorePage() {
               justifyContent: "center",
             }}
           >
-            <h1>@{profile?.data?.data?.username}</h1>
+            <h1>@{store?.store?.store?.username}</h1>
 
             <VerifiedIcon
               style={{
