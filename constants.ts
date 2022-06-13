68273080a79a099ced9950f93660ec11841bf9a6
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-06-14 01:33:23 +0530

    Update meili url

--- constants.ts
+++ constants.ts
@@ -48,9 +48,9 @@ export const SAVE_PRODUCT = "product/save";
 
 // Search
 export const MEILI_URL = ISPROD
-  ? "http://52.66.214.126/"
+  ? "http://43.204.122.133/"
   : "http://127.0.0.1:7700";
-export const MEILI_API_KEY = "masteKey";
+export const MEILI_API_KEY = ISPROD ? process.env.MEILI_KEY : "masteKey";
 
 // TEST
 export const SEND_MAIL = "test/sendMail";
