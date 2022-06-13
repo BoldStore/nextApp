bc18908b783dad896dd40a95321394d03f6bf174
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-06-13 18:13:57 +0530

    Fix load

--- components/RouteGuard/index.tsx
+++ components/RouteGuard/index.tsx
@@ -9,24 +9,21 @@ export default function RouteGuard({ children }: any) {
   const [authorized, setAuthorized] = useState(false);
 
   useEffect(() => {
-    if (!loading) {
-      // on initial load - run auth check
-      authCheck(router.asPath);
+    // on initial load - run auth check
+    authCheck(router.asPath);
 
-      // on route change start - hide page content by setting authorized to false
-      const hideContent = () => setAuthorized(false);
-      router.events.on("routeChangeStart", hideContent);
+    // on route change start - hide page content by setting authorized to false
+    const hideContent = () => setAuthorized(false);
+    router.events.on("routeChangeStart", hideContent);
 
-      // on route change complete - run auth check
-      router.events.on("routeChangeComplete", authCheck);
+    // on route change complete - run auth check
+    router.events.on("routeChangeComplete", authCheck);
 
-      // unsubscribe from events in useEffect return function
-      return () => {
-        router.events.off("routeChangeStart", hideContent);
-        router.events.off("routeChangeComplete", authCheck);
-      };
-    }
-
+    // unsubscribe from events in useEffect return function
+    return () => {
+      router.events.off("routeChangeStart", hideContent);
+      router.events.off("routeChangeComplete", authCheck);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
 
