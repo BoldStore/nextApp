bb8a38f99875eb6f3d5bd77f8fe882a07d7222b2
aryanteng <aryanteng892@gmail.com>
2022-06-14 14:47:37 +0530

    mobile header fixes

--- components/CustomerComponents/Header/DrawerComponent.js
+++ components/CustomerComponents/Header/DrawerComponent.js
@@ -64,6 +64,9 @@ export default function TemporaryDrawer() {
               viewBox="0 0 24 24"
               width="24"
               xmlns="http://www.w3.org/2000/svg"
+              style={{
+                marginBottom: "-0.3rem",
+              }}
             >
               <path
                 d="M4 8H20M4 16H20"
