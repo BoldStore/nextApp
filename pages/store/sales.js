6711003f3fdec5ce62b80d3516e90aa6fccfa36b
avivashishta29 <avivashishta@gmail.com>
2022-06-30 13:38:12 -0400

    dashboard done

--- pages/store/sales.js
+++ pages/store/sales.js
@@ -7,7 +7,39 @@ function Sales() {
     <div>
       <Header />
       <div className={styles.container}>
-        <h1>Your Sales</h1>
+        <h1>Pending Sales</h1>
+        <div className={styles.tableContainer}>
+          <table className={styles.table}>
+            <tr>
+              <th>Order ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Customer</th>
+              <th>Date</th>
+              <th>Payment Status</th>
+              <th>Payment Id</th>
+              <th>Shiprocket Status</th>
+              <th>Shiprocket Id</th>
+            </tr>
+            <tr>
+              <td>12345VHW</td>
+              <td>Jeans</td>
+              <td>$2200</td>
+              <td>
+                <a href="https://www.instagram.com/avi_vashishta">
+                  @avi_vashishta
+                </a>
+              </td>
+              <td>22/07/22</td>
+              <td>Completed</td>
+              <td>12345VHW</td>
+              <td>Delivery Pending</td>
+              <td>1234NQWHW</td>
+            </tr>
+          </table>
+        </div>
+
+        <h1>Completed Sales</h1>
         <div className={styles.tableContainer}>
           <table className={styles.table}>
             <tr>
