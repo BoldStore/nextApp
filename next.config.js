3628570e7b7c6972959b0b8df113c26d347165e5
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-07-01 19:51:23 +0530

    Config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      "products-boldstore.s3.ap-south-1.amazonaws.com",
-      "profile-pics-boldstore.s3.ap-south-1.amazonaws.com",
+      "products-bold.s3.ap-south-1.amazonaws.com",
+      "profile-pics-bold.s3.ap-south-1.amazonaws.com",
     ],
   },
 };
