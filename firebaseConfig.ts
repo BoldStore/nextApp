b29dba73ffffeb510a98a4424ec9072659aded77
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-07-01 19:45:24 +0530

    Dev

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,19 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCPHtJ__EEMJdF89LiLSpCWR91Osug3Da8",
-  authDomain: "bold-a1e8e.firebaseapp.com",
-  projectId: "bold-a1e8e",
-  storageBucket: "bold-a1e8e.appspot.com",
-  messagingSenderId: "739646855630",
-  appId: "1:739646855630:web:f1ba524b76af5010bb339f",
-  measurementId: "G-44BM9FKS2Z",
+  apiKey: "AIzaSyC58rnwX7Zas-GdxfettF49Qjh6J6vSPAY",
+  authDomain: "bold-96a92.firebaseapp.com",
+  projectId: "bold-96a92",
+  storageBucket: "bold-96a92.appspot.com",
+  messagingSenderId: "1033134164289",
+  appId: "1:1033134164289:web:4f7675c97c498d5a5879c0",
+  measurementId: "G-TRWHN385YE",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
+// To use firebase emulator
+// connectAuthEmulator(auth, "http://localhost:9099");
