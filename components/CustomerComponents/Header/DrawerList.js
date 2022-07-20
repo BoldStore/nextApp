2709f6ce6699dc74d03a44046602baf273dd9c22
avivashishta29 <avivashishta@gmail.com>
2022-07-20 11:02:52 -0400

    login icon

--- components/CustomerComponents/Header/DrawerList.js
+++ components/CustomerComponents/Header/DrawerList.js
@@ -10,6 +10,7 @@ import {
   Settings,
   LogOut,
   Bookmark,
+  LogIn,
 } from "react-feather";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../../firebaseConfig";
@@ -150,6 +151,7 @@ function DrawerList() {
             style={{ margin: "1rem" }}
             onClick={login}
           >
+            <LogIn className={styles.icon} />
             Login
           </p>
         )}
